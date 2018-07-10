38d2c17264dcd220c3244cb021214af57a62f1dc
unknown <third1337@gmail.com>
2018-07-10 19:20:13 +0700

    hapus ikon search pada header

--- resources/views/merchant/partials/header.blade.php
+++ resources/views/merchant/partials/header.blade.php
@@ -54,12 +54,12 @@
                         <i class="dripicons-menu"></i>
                     </button>
                 </li>
-                <li class="hide-phone app-search">
+               <!--  <li class="hide-phone app-search">
                     <form role="search" class="">
                         <input type="text" placeholder="Search..." class="form-control">
                         <a href=""><i class="fa fa-search"></i></a>
                     </form>
-                </li>
+                </li> -->
             </ul>
 
         </nav>
